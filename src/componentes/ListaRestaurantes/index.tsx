9fd909157d3b1445cc3c9c923afed2c2ecaeac36
fladeia <ladeiadev@gmail.com>
2022-10-12 12:38:25 -0300

    added search field

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,104 +1,24 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import axios, { AxiosRequestConfig } from 'axios';
+import { Button, TextField } from '@mui/material';
 import IRestaurante from '../../interfaces/IRestaurante';
 import iPaginacao from '../../interfaces/IPaginacao'
-import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+import style from './ListaRestaurantes.module.scss';
+
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
 
 const ListaRestaurantes = () => {
-  const restauratsUrl = 'http://localhost:8000/api/v1/restaurantes/'
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
-  // const [nextPage, setNextPage] = useState('')
   const [nextPage, setNextPage] = useState('')
   const [prevPage, setPrevPage] = useState('')
+  const [searchRestaurant, setSearchRestaurant] = useState('')
 
-  // const restaurantes: IRestaurante[] = [
-  //   {
-  //     id: 1,
-  //     nome: "Lyllys Cafe",
-  //     pratos: [
-  //       {
-  //         id: 1,
-  //         descricao: 'Lasanha à Bolonhesa',
-  //         imagem: 'https://receitassaborosa.com/wp-content/uploads/2019/12/Lasanha-com-Molho-a-Bolonhesa.jpg',
-  //         nome: 'Lasanha',
-  //         restaurante: 1,
-  //         tag: 'Italiana'
-  //       },
-  //       {
-  //         id: 2,
-  //         descricao: 'Strogonoff de Frango à brasileira',
-  //         imagem: 'https://img.itdg.com.br/images/recipes/000/002/462/332854/332854_original.jpg',
-  //         nome: 'Strogonoff',
-  //         restaurante: 1,
-  //         tag: 'Russa'
-  //       },
-  //       {
-  //         id: 3,
-  //         descricao: 'Empadão de Frango',
-  //         imagem: 'https://img.itdg.com.br/tdg/images/recipes/000/008/054/38915/38915_original.jpg?mode=crop&width=710&height=400',
-  //         nome: 'Empadão de Frango',
-  //         restaurante: 1,
-  //         tag: 'Portuguesa'
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     id: 2,
-  //     nome: "Sugiro Sushi",
-  //     pratos: [
-  //       {
-  //         id: 1,
-  //         descricao: 'Combinado de 8 peças',
-  //         imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/5031e263a4a258791d6306b2d3d9dbf6_XL.jpg',
-  //         nome: 'Sushi',
-  //         restaurante: 1,
-  //         tag: 'Japonesa'
-  //       },
-  //       {
-  //         id: 2,
-  //         descricao: 'Sashimi de Salmão',
-  //         imagem: 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2009/04/sashimi_facil.jpg',
-  //         nome: 'Sashimi',
-  //         restaurante: 1,
-  //         tag: 'Japonesa'
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     id: 3,
-  //     nome: "Cantina da Escola",
-  //     pratos: [
-  //       {
-  //         id: 1,
-  //         descricao: 'Salgado de queijo com presunto',
-  //         imagem: 'https://img.itdg.com.br/tdg/images/recipes/000/102/312/279767/279767_original.jpg',
-  //         nome: 'Quejunto',
-  //         restaurante: 1,
-  //         tag: 'Lanche'
-  //       },
-  //       {
-  //         id: 2,
-  //         descricao: 'Coxinha de Frango',
-  //         imagem: 'https://t1.rg.ltmcdn.com/pt/posts/1/9/1/coxinha_simples_191_600.jpg',
-  //         nome: 'Coxinha',
-  //         restaurante: 1,
-  //         tag: 'Lanche'
-  //       },
-  //       {
-  //         id: 3,
-  //         descricao: 'Risole de Palmito',
-  //         imagem: 'https://img.itdg.com.br/tdg/images/recipes/000/005/116/323871/323871_original.jpg',
-  //         nome: 'Risole',
-  //         restaurante: 1,
-  //         tag: 'Lanche'
-  //       }
-  //     ]
-  //   }
-  // ]
-
-  function seeMore(url: string) {
-    axios.get<iPaginacao<IRestaurante>>(url)
+  function seeMore(url: string, options: AxiosRequestConfig = {}) {
+    axios.get<iPaginacao<IRestaurante>>(url, options)
       .then(response => {
         setRestaurantes(response.data.results)
         setNextPage(response.data.next)
@@ -107,12 +27,48 @@ const ListaRestaurantes = () => {
       .catch(err => console.log(err))
   }
 
+  function Search(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const options = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (searchRestaurant) {
+      options.params.search = searchRestaurant
+    }
+    seeMore('http://localhost:8000/api/v1/restaurantes/', options)
+  }
+
   useEffect(() => {
-    seeMore(restauratsUrl)
+    seeMore('http://localhost:8000/api/v1/restaurantes/')
   }, [])
 
+  console.log(searchRestaurant)
+
   return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <div className={style.flex}>
+      <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+      <form
+        className={style.flex}
+        onSubmit={Search}
+      >
+        <TextField
+          id="standard-basic"
+          label="Buscar Restaurante"
+          variant="standard"
+          value={searchRestaurant}
+          onChange={event => setSearchRestaurant(event?.target.value)}
+        />
+        <Button
+          variant="text"
+          type='submit'
+        >
+          Buscar
+        </Button>
+      </form>
+    </div>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     <button onClick={() => seeMore(nextPage)} disabled={!nextPage}>Próxima</button>
     <button onClick={() => seeMore(prevPage)} disabled={!prevPage}>Anterior</button>
